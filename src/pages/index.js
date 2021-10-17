6c4b05311eb6847eabc81212d4d389f1be2a04f3
Zenky <bzenky@gmail.com>
2021-10-17 09:44:03 -0300

    favicon, lighthouse fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,8 @@ export default function Home() {
     <div className={styles.app}>
       <Head>
         <title>Draco Price</title>
-        <meta name="description" content="Draco Price" />
+        <meta name="description" content="Simple Page with real time Draco Price." />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
