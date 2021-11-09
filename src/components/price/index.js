3a03cc94efae2036770b4558b3628504fc371693
Zenky <bzenky@gmail.com>
2021-11-08 22:14:55 -0300

    updated Derby, util

--- src/components/price/index.js
+++ src/components/price/index.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react'
 import axios from 'axios'
 import { format } from 'date-fns'
+import { formatDerby, formatPrice } from '../../util/format'
 
 import styles from './styles.module.scss'
 
@@ -8,24 +9,33 @@ export function Price() {
 
   const date = format(new Date(), 'HH:mm (dd-MM-yyyy)')
   const [dracoUSD, setDracoUSD] = useState('')
+  const [derbyDS, setDerbyDS] = useState(0)
 
   useEffect(() => {
     axios.post('https://api.mir4global.com/wallet/prices/draco/lastest')
       .then(res => {
-        const formatedDracoUSD = (value) =>
-          new Intl.NumberFormat('en-US', {
-            style: 'currency',
-            currency: 'USD'
-          }).format(value)
+        const updatedDraco = res.data.Data.USDDracoRate
 
-        setDracoUSD(formatedDracoUSD(res.data.Data.USDDracoRate))
+        setDracoUSD(formatPrice(updatedDraco))
+      })
+  }, [])
+
+  useEffect(() => {
+    axios.post('https://api.mir4global.com/wallet/prices/derby')
+      .then(data => {
+
+        const lastestDerby = data.data.Data
+        const updatedDayDerby = lastestDerby[lastestDerby.length - 1].DS
+
+        setDerbyDS(formatDerby(updatedDayDerby))
       })
   }, [])
 
 
   return (
     <div className={styles.content}>
-      <span className={styles.value}>USD {dracoUSD}</span>
+      <span className={styles.dracoValue}>USD {dracoUSD}</span>
+      <span className={styles.derbyValue}>Derby {derbyDS}</span>
       <time className={styles.date}>
         <span>Updated at {date}</span>
       </time>
